c2fd732cd6b976f2f9793ace8bae4e2fdfb67272
Hossain Saddam <hossaincrr7@gmail.com>
2021-06-25 02:20:11 +0600

    update hero and WIP transition-timing-function

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,15 +1,33 @@
-import {
-  Swiper,
-  Autoplay,
-  EffectFade,
-} from 'swiper';
-Swiper.use([Autoplay, EffectFade])
+const sliderItems = document.querySelectorAll('.o-hero__slider-item')
+const banners = document.querySelectorAll('.o-banner')
+let index = 0
+sliderItems[0].classList.add('is-active')
+banners[0].classList.add('is-active')
+
+function slider() {
+  index++
+
+  if (index == sliderItems.length) {
+    index = 0
+  }
+
+  sliderItems[index].classList.add('is-active')
+  banners[index].classList.add('is-active')
+
+  let prevIndex = index - 1
+
+  if (prevIndex == -1) {
+    prevIndex = sliderItems.length - 1
+
+    sliderItems[prevIndex].classList.remove('is-active')
+    banners[prevIndex].classList.remove('is-active')
+  }
+
+  setTimeout(() => {
+    sliderItems[prevIndex].classList.remove('is-active')
+    banners[prevIndex].classList.remove('is-active')
+  }, 1000)
+}
 
 
-var swiper = new Swiper(".o-hero__slider", {
-  effect: "fade",
-  autoplay: true,
-  wrapperClass: 'o-hero__slider-container',
-  slideClass: 'o-hero__slider-item',
-  slideActiveClass: 'is-active',
-})
+setInterval(slider, 4000)
