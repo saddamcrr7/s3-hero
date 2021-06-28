33ffbc611dcaac867f4878589c908dfbe6e5515d
Hossain Saddam <hossaincrr7@gmail.com>
2021-06-28 09:36:58 +0600

    fix logo scrolling effect

--- src/scripts/brand.js
+++ src/scripts/brand.js
@@ -12,11 +12,12 @@ const split2 = document.querySelector('.c-brand__split--2 .c-brand__image')
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: "#main",
-    start: "top top",
-    end: "bottom bottom",
+    start: "2px top",
+    end: `3000px bottom`,
     scrub: true,
+    markers: true
   }
 });
 
-tl.to(split1, {translateX: 100, translateY: 200, duration: 1})
-tl.to(split2, {translateX: -100, translateY: -200, duration: 1, delay: -1})
+tl.to(split1, {translateX: 100, translateY: 100, duration: 1})
+tl.to(split2, {translateX: -100, translateY: -100, duration: 1, delay: -1})
