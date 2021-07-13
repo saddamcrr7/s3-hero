ec478e3e1041ccd9e70221816812b4e2d09f4af8
Hossain Saddam <hossaincrr7@gmail.com>
2021-07-13 22:37:14 +0600

    fix load event

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -1,6 +1,6 @@
 const $preloader = document.querySelector('.js-preloader')
 
-window.addEventListener('DOMContentLoaded',()=> {
+window.addEventListener('load',()=> {
   $preloader.style.opacity = '0'
 
   $preloader.addEventListener('transitionend', ()=> {
