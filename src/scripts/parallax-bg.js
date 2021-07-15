e55cb8f4f127c7aa78c67ef7a8950fb46f95f32f
Hossain Saddam <hossaincrr7@gmail.com>
2021-07-15 18:31:48 +0600

    home page fixes

--- src/scripts/parallax-bg.js
+++ src/scripts/parallax-bg.js
@@ -4,25 +4,63 @@ import {
 } from "../scripts/vendor/gsap-member/src/all";
 
 
-window.addEventListener('load', ()=> {parallaxBg()})
+window.addEventListener('load', () => {
+  parallaxBg()
+})
 
 function parallaxBg() {
   gsap.utils.toArray(".o-bg").forEach((section, i) => {
     section.bg = section.querySelector(".o-bg__image");
+    section.bgOB = section.querySelector(".o-bg__image--obpyb");
 
-    section.bg.style.objectPosition =
-    `50% ${-section.clientHeight/ 1.2}px`;
+    if (section.bgOB) {
 
-    gsap.to(section.bg, {
-      objectPosition: `50% ${section.clientHeight/2}px`,
-      ease: "none",
-      scrollTrigger: {
-        trigger: section,
-        start: "top bottom",
-        end: "bottom top-=100%",
-        scrub: true
+
+
+
+      function myFunction(x) {
+        if (x.matches) {
+          section.bgOB.style.objectPosition =
+            `50% ${-section.clientHeight * 2.2}px`;
+        } else {
+          section.bgOB.style.objectPosition =
+            `50% ${-section.clientHeight * 3}px`;
+        }
       }
-    });
+
+      let x = window.matchMedia("(max-width: 1440px)")
+      myFunction(x)
+      x.addListener(myFunction)
+
+
+      gsap.to(section.bgOB, {
+        objectPosition: `50% ${section.clientHeight}px`,
+        ease: "none",
+        scrollTrigger: {
+          trigger: section,
+          start: "top bottom",
+          end: "bottom top-=100%",
+          scrub: true
+        }
+      });
+
+
+    } else {
+      section.bg.style.objectPosition =
+        `50% ${-section.clientHeight/ 1.2}px`;
+
+      gsap.to(section.bg, {
+        objectPosition: `50% ${section.clientHeight/2}px`,
+        ease: "none",
+        scrollTrigger: {
+          trigger: section,
+          start: "top bottom",
+          end: "bottom top-=100%",
+          scrub: true
+        }
+      });
+    }
+
+
   });
 }
-
