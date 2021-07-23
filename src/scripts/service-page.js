a46df05b7285c1fe4324a0446043ca77d62ea575
Hossain Saddam <hossaincrr7@gmail.com>
2021-07-23 21:24:05 +0600

    WIP service page

--- src/scripts/service-page.js
+++ src/scripts/service-page.js
@@ -11,67 +11,106 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const $hero = document.querySelector('#service-hero')
 
+function servicesSctions() {
+  if ($hero) {
+    const $heroTiltle = document.querySelector('#service-hero-title')
+    const $heroTiltleBg = $heroTiltle.querySelector('.c-service-hero-title__bg')
+    const $heroTitleSpacer = $hero.querySelector('.o-simple-hero__title-spacer')
+    const $servicesContainer = document.querySelector('#services-container')
+    const $services = document.querySelectorAll('.o-service')
 
-if ($hero) {
-  const $heroTiltle = document.querySelector('#service-hero-title')
-  const $heroTiltleBg = $heroTiltle.querySelector('.c-service-hero-title__bg')
-  const $heroTitleSpacer = $hero.querySelector('.o-simple-hero__title-spacer')
-  const $servicesContainer = document.querySelector('#services-container')
-  const $services = document.querySelectorAll('.o-service')
+    $heroTiltle.style.top = `${getPosition($heroTitleSpacer).top}px`
+    $heroTiltle.style.left = `${getPosition($heroTitleSpacer).left - 20}px`
 
-  $heroTiltle.style.top = `${getPosition($heroTitleSpacer).top}px`
-  $heroTiltle.style.left = `${getPosition($heroTitleSpacer).left - 20}px`
+    $heroTiltleBg.style.top = `-${getPosition($heroTitleSpacer).top}px`
+    $heroTiltleBg.style.left = `-${getPosition($heroTitleSpacer).left-16}px`
 
-  $heroTiltleBg.style.top = `-${getPosition($heroTitleSpacer).top}px`
-  $heroTiltleBg.style.left = `-${getPosition($heroTitleSpacer).left-16}px`
+    gsap.to($heroTiltle, {
+      top: 100,
+      ease: 'none',
 
-  gsap.to($heroTiltle, {
-    top: 100,
-    ease: 'none',
+      scrollTrigger: {
+        trigger: $servicesContainer,
+        start: "top+=50 center",
+        end: "500 center",
+        scrub: true,
+      }
 
-    scrollTrigger: {
-      trigger: $servicesContainer,
-      start: "top+=50 center",
-      end: "500 center",
-      scrub: 0.7,
-    }
-
-  });
-
-  ScrollTrigger.defaults({
-    toggleActions: "restart pause resume pause"
-  });
-
-  const firstElem = document.querySelector(".o-service");
-
-  function goToSection(i, anim) {
-    gsap.to(window, {
-      scrollTo: {
-        y: i * innerHeight + firstElem.offsetTop,
-        autoKill: false,
-        // overwrite: true,
-      },
-      // duration: 1,
-      ease: 'none'
     });
 
-    if (anim) {
-      anim.restart();
+    ScrollTrigger.defaults({
+      toggleActions: "restart pause resume pause"
+    });
+
+
+    function goToSection(section, i, anim) {
+
+
+      console.log(i);
+      gsap.to(window, {
+        scrollTo: {
+          y: i * section.clientHeight,
+          autoKill: false
+        },
+        ease: 'none'
+      });
+      if (anim) {
+        anim.restart();
+      }
     }
+
+
+    $services.forEach((section, i) => {
+      const $serviceContent = section.querySelector('.o-service__content')
+
+      i += 1
+
+
+      ScrollTrigger.create({
+        trigger: section,
+        pinSpacing: false,
+
+        onEnter: () => goToSection(section, i)
+      });
+
+      ScrollTrigger.create({
+        trigger: section,
+        start: "bottom bottom",
+        pinSpacing: false,
+
+        onEnterBack: () => goToSection(section, i),
+      });
+
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        end: "bottom top",
+        scrub: false,
+        pin: true,
+        pinSpacing: false,
+      });
+
+
+      gsap.to($serviceContent, {
+        opacity: 1,
+
+        scrollTrigger: {
+          trigger: section,
+          start: "top center",
+          end: "bottom bottom",
+          scrub: true,
+          markers: true
+        }
+
+      });
+
+    });
+
+
   }
 
-  $services.forEach((panel, i) => {
-    // ScrollTrigger.create({
-    //   trigger: panel,
-    //   onEnter: () => goToSection(i)
-    // });
-
-    // ScrollTrigger.create({
-    //   trigger: panel,
-    //   start: "bottom bottom",
-    //   onEnterBack: () => goToSection(i),
-    // });
-
-  });
-
 }
+
+window.addEventListener('load', () => {
+  servicesSctions()
+})
