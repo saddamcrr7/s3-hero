3f3acbe003dc7817b85628fe67fd3ffca2002b17
Hossain Saddam <hossaincrr7@gmail.com>
2021-08-09 22:20:51 +0600

    update meet3 and project

--- src/scripts/cover.js
+++ src/scripts/cover.js
@@ -3,28 +3,48 @@ import {
   ScrollTrigger,
 } from "../scripts/vendor/gsap-member/src/all";
 
+import getPosition from './utils/getPosition'
+
 gsap.registerPlugin(ScrollTrigger);
 
 function cover() {
-  const $covers = document.querySelectorAll('.o-cover')
+  const $serviceCover = document.querySelector('.o-cover--service')
+  const $faqCover = document.querySelector('.o-cover--faq')
 
-  if ($covers.length) {
-    $covers.forEach(cover => {
-      cover.titles = cover.querySelectorAll('.o-cover__title')
+  if ($serviceCover) {
+    $serviceCover.titles = $serviceCover.querySelectorAll('.o-cover__title')
 
-      cover.titles.forEach(title => {
+    $serviceCover.titles.forEach(title => {
         gsap.from(title, {
           scale: 0,
           ease: 'back.out',
           scrollTrigger: {
-            trigger: cover,
+            trigger: $serviceCover,
             start: "top center",
             end: "bottom bottom",
             scrub: true,
           }
         })
       })
+  }
+
+
+  if($faqCover) {
+    $faqCover.tringle = $faqCover.querySelector('.o-cover__triangle')
+    $faqCover.content = $faqCover.querySelector('.o-cover__content')
+    $faqCover.faqs = $faqCover.querySelectorAll('.c-faq')
+
+    gsap.set($faqCover.tringle, {left: getPosition($faqCover.faqs[0]).left + ($faqCover.faqs[0].clientWidth /  2 - ($faqCover.tringle.clientWidth / 2))})
+
+    $faqCover.faqs.forEach(faq => {
+      faq.addEventListener('mouseenter', ()=> {
+        gsap.to($faqCover.tringle, {
+          left: getPosition(faq).left + (faq.clientWidth /  2 - ($faqCover.tringle.clientWidth / 2)),
+          ease: 'none'
+        })
+      })
     })
+
   }
 
 }
--- src/scripts/timeline.js
+++ src/scripts/timeline.js
@@ -6,46 +6,54 @@ import {
 
 gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener('load', ()=> {timeline()})
+window.addEventListener('load', () => {
+  timeline()
+})
 
 function timeline() {
+  const $timeLine = document.querySelector('.o-timeline')
 
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".o-timeline",
-      start: "top center",
-      end: "bottom center",
-      scrub: true,
-    }
-  });
-
-  tl.to('.o-timeline__progress-bar', {
-    ease: 'none',
-    height: '100%'
-  })
-
-
-  gsap.utils.toArray(".o-timeline__line-inner").forEach((line, i) => {
-    line.year = line.querySelector('.o-timeline__year')
-
-    gsap.to(line.year, {
-      y: line.clientHeight - 50,
-      ease: 'none',
-
+  if ($timeLine) {
+    const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: line,
+        trigger: ".o-timeline",
         start: "top center",
         end: "bottom center",
         scrub: true,
-        onEnter: ()=> {line.year.classList.add('is-active')},
-        onLeaveBack: ()=> {line.year.classList.remove('is-active')},
-      },
-
-      onUpdate: ScrollTrigger.update
-
+      }
     });
 
-  });
+    tl.to('.o-timeline__progress-bar', {
+      ease: 'none',
+      height: '100%'
+    })
 
+
+    gsap.utils.toArray(".o-timeline__line-inner").forEach((line, i) => {
+      line.year = line.querySelector('.o-timeline__year')
+
+      gsap.to(line.year, {
+        y: line.clientHeight - 50,
+        ease: 'none',
+
+        scrollTrigger: {
+          trigger: line,
+          start: "top center",
+          end: "bottom center",
+          scrub: true,
+          onEnter: () => {
+            line.year.classList.add('is-active')
+          },
+          onLeaveBack: () => {
+            line.year.classList.remove('is-active')
+          },
+        },
+
+        onUpdate: ScrollTrigger.update
+
+      });
+
+    });
+  }
 
 }
