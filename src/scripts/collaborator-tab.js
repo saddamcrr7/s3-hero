ede76322310c7d4e8ae1932d029575cf52f75132
Hossain <hossaincrr7@gmail.com>
2021-12-12 08:58:48 +0600

    add personal tab images

--- src/scripts/collaborator-tab.js
+++ src/scripts/collaborator-tab.js
@@ -1,24 +1,85 @@
-const $tabs = document.querySelectorAll('.c-tab')
 
-$tabs.forEach( $tab => {
-  const $btns = $tab.querySelectorAll('.c-tab__btn')
-  const $panels = $tab.querySelectorAll('.c-tab__panel')
-  const $body = $tab.querySelector('.c-tab__body')
+import {
+  gsap,
+} from "../scripts/vendor/gsap-member/src/all";
 
-  $body.style.height = `${$panels[0].querySelector('.c-tab__panel-inner').clientHeight}px`
+window.addEventListener('load',()=> {
+  const $tabs = document.querySelectorAll('.c-tab')
+  const $images = document.querySelectorAll('.c-collaborator-modal__images')
 
-  $btns.forEach(($btn, i) => {
-    $btn.addEventListener('click', ()=> {
-      $btns.forEach(btn =>  btn.classList.remove('is-active'))
-      $panels.forEach(panel =>  panel.classList.remove('is-active'))
+  $tabs.forEach( ($tab, j)=> {
+    const $btns = $tab.querySelectorAll('.c-tab__btn')
+    const $panels = $tab.querySelectorAll('.c-tab__panel')
+    const $body = $tab.querySelector('.c-tab__body')
+    let $imageWrappers
 
-       $btn.classList.add('is-active')
-       $panels[i].classList.add('is-active')
+    setTimeout(() => {
+      $body.style.height = `${$panels[0].querySelector('.c-tab__panel-inner').clientHeight}px`
+    }, 1000);
 
-       const $panelInner = $panels[i].querySelector('.c-tab__panel-inner')
+    if(j < $images.length) {
+      $imageWrappers = $images[j].querySelectorAll('.c-collaborator-modal__image-wrapper')
 
-       $body.style.height = `${$panelInner.clientHeight}px`
+      gsap.set($imageWrappers[1], {
+        height: 0,
+        zIndex: 0
+      })
+
+      setTimeout(() => {
+        gsap.set($images[j], {
+          height: $imageWrappers[0].querySelector('.c-collaborator-modal__image').offsetHeight
+        })
+        gsap.set($imageWrappers[0], {
+          height: $imageWrappers[0].querySelector('.c-collaborator-modal__image').offsetHeight
+        })
+      }, 1000);
+    }
+
+    $btns.forEach(($btn, i) => {
+      $btn.addEventListener('click', ()=> {
+        $btns.forEach(btn =>  btn.classList.remove('is-active'))
+        $panels.forEach(panel =>  panel.classList.remove('is-active'))
+
+         $btn.classList.add('is-active')
+         $panels[i].classList.add('is-active')
+
+         const $panelInner = $panels[i].querySelector('.c-tab__panel-inner')
+
+         $body.style.height = `${$panelInner.clientHeight}px`
+
+         if(j < $images.length) {
+           let preI = i - 1
+           if (preI == -1) {
+             preI = $btns.length - 1
+           }
+
+          const indexImageHeight = $imageWrappers[i].querySelector('.c-collaborator-modal__image').offsetHeight
+
+          gsap.to($images[j], {
+            height: indexImageHeight
+          })
+
+          gsap.set($imageWrappers[preI], {
+            zIndex: 0
+          })
+
+          gsap.set($imageWrappers[i], {
+            zIndex: 1
+          })
+
+          gsap.to($imageWrappers[i], {
+            height: indexImageHeight,
+
+            onComplete: ()=> {
+              gsap.set($imageWrappers[preI], {
+                height: 0,
+              })
+            }
+          })
+         }
+      })
     })
+
   })
 
 })
