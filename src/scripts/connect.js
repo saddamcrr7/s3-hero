8137b337f72b78ee27e952bec43d1ed5f9b4933d
Hossain <hossaincrr7@gmail.com>
2021-12-15 20:55:13 +0600

    update connect page

--- src/scripts/connect.js
+++ src/scripts/connect.js
@@ -1,10 +1,13 @@
 
 window.addEventListener('load', ()=>{
   const $tooltips =  document.querySelectorAll('.o-connect__contact-tooltip')
+  const $icons = document.querySelectorAll('.o-connect__contact-icon')
 
   if($tooltips.length) {
-    $tooltips.forEach(tooltip => {
-      tooltip.style.left = `-${tooltip.offsetWidth / 2}px`
-    })
+    setTimeout(() => {
+      $tooltips.forEach((tooltip, i) => {
+        tooltip.style.left = `-${tooltip.clientWidth / 2 - $icons[i].clientWidth / 2}px`
+      })
+    }, 1000);
   }
 })
