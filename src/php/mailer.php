f7ed102079c285e1af13f7022979ac08e6bf3bae
Hossain <hossaincrr7@gmail.com>
2021-12-19 20:18:47 +0600

    update mailer

--- src/php/mailer.php
+++ src/php/mailer.php
@@ -1,87 +1,76 @@
 <?php
-/*-----------------------------------------------
-	# Variables
-	---------------------------------------------*/
+if (isset($_POST['Email'])) {
 
-$name       = $_POST['name'];
-$email      = $_POST['email'];
-$subject    = isset($_POST['subject']) && !empty($_POST['subject']) ? $_POST['subject'] : 'New message from your site contact form';
-$content    = $_POST['content'];
-$toMail     = 'Hossain Saddam <hossaincrr7@gmail.com>'; // Your name & mail address here example 'Your Name <contact@domain.com>'.
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "marat@s-3.co";
+    $email_subject = "Connect Mail from S3";
 
-/*-----------------------------------------------
-	# Error Reporting need first
-	---------------------------------------------*/
-$error      = false;
-$msg        = '';
-$body       = '';
+    function problem($error)
+    {
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
 
-// Check Name
-if (empty($name)) {
-	$error = true;
-	$msg   .= '<strong>Required:</strong> Please enter your name.';
-	$msg   .= '<br>';
-} else {
-	$body  .= '<strong>Name:</strong> ' . $name;
-	$body  .= '<br><br>';
+    // validation expected data exists
+    if (
+        !isset($_POST['Name']) ||
+        !isset($_POST['Email']) ||
+        !isset($_POST['Message'])
+    ) {
+        problem('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $name = $_POST['Name']; // required
+    $email = $_POST['Email']; // required
+    $message = $_POST['Message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- include your success message below -->
+
+    Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
 }
-
-// Check Email
-if (empty($email)) {
-	$error = true;
-	$msg   .= '<strong>Required:</strong> Please enter your valid email address.';
-	$msg   .= '<br>';
-} else {
-	$body  .= '<strong>Email:</strong> ' . $email;
-	$body  .= '<br><br>';
-}
-
-// Check Content
-if (empty($content)) {
-	$error = true;
-	$msg   .= '<strong>Required: </strong> Please write something. Can\'t here you from our home';
-	$msg   .= '<br>';
-} else {
-	// Subject
-	$body  .= '<strong>Subject:</strong> ' . $subject;
-	$body  .= '<br><br>';
-
-	// Body Content
-	$body  .= '<strong>Message:</strong> ' . $content;
-	$body  .= '<br><br>';
-}
-
-/*-----------------------------------------------
-	# Prepare send mail
-	---------------------------------------------*/
-if ($error == true) {
-	$msg    .= '<strong>Error:</strong> Please fill form with above info requirement.';
-} else {
-	$body   .= $_SERVER['HTTP_REFERER'] ? '<br><br><br>This form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
-	$error   = false;
-	$msg    .= '<strong>Success:</strong> Your message has been send.';
-
-	// Mail Headers
-	$headers   = array();
-	$headers[] = "MIME-Version: 1.0";
-	$headers[] = "Content-type: text/html; charset=iso-8859-1";
-	$headers[] = "From: {$name} <{$email}>";
-	$headers[] = "Reply-To: {$name} <{$email}>";
-	$headers[] = "Subject: {$subject}";
-	$headers[] = "X-Mailer: PHP/".phpversion();
-
-	mail($toMail, $subject, $body, implode("\r\n", $headers));
-}
-// Make as json obj
-$dataReturn = array(
-	'error'   => $error,
-	'message'   => $msg,
-	'data'  => array(
-		'name' => $name,
-		'email' => $email,
-		'subject' => $subject,
-		'content' => $content
-	)
-);
-header('Content-type: application/json');
-echo json_encode($dataReturn);
+?>
